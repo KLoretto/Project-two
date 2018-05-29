bab743215baec4aeee7fb8fe31dcf46844751bd0
Sol <solomon.miller@gmail.com>
2018-05-28 21:51:47 -0500

    added functionality to map to allow for filtering between categories

--- logic.js
+++ logic.js
@@ -1,139 +1,280 @@
-// Create a map object
-var myMap = L.map("map", {
-  center: [37.983810, 23.727539],
-  zoom: 2
-});
 
-// Add a tile layer
-L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?" +
-"access_token=pk.eyJ1Ijoic29sb21vbm1pbGxlciIsImEiOiJjamd4MDduZ2QxdjNtMnFtcWticW4yZDdrIn0.UzUscd_xQuIDVRDmh2CZjw"
-).addTo(myMap);
 
 // An array containing each city's name, location, and Actor
 var cities = [{
   location: [51.509865, -0.118092],
+  location1: [51.509865, -0.118092],
+  location2: [51.509865, -0.118092],
   name: "New Cross, London",
-  Actor: "Gary Oldman"
+  name1: "New Cross, London",
+  name2: "New Cross, London",
+  Actor: "Gary Oldman",
+  Actor1: "Gary Oldman",
+  Actor2: "Gary Oldman"
 },
 {
   location: [41.5532, -70.6086],
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
   name: "Falmouth",
-  Actor: "Casey Affleck"
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
 },
 {
   location: [34.0928, -118.3287],
+  location1: [34.0928, -118.3287],
+  location2: [34.0928, -118.3287],
   name: "Hollywood",
-  Actor: "Leonardo DiCaprio"
+  name1: "Hollywood",
+  name2: "Hollywood",
+  Actor: "Leonardo DiCaprio",
+  Actor1: "Leonardo DiCaprio",
+  Actor2: "Leonardo DiCaprio"
 },
 {
   location: [51.4975, -0.1357],
-  name: "Westminster, London",
-  Actor: "Eddie Redmayne"
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
 },
 {
   location: [29.2097, -99.7862],
-  name: "Uvalde",
-  Actor: "Matthew McConaughey"
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
 },
 {
   location: [51.5074, -0.1278],
-  name: "London",
-  Actor: "Daniel Day-Lewis"
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
 },
 {
   location: [48.8285, 2.2188],
-  name: "Hauts-De-Seine",
-  Actor: "Jean Dujardin"
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
 },
 {
   location: [51.0577, 1.3081],
-  name: "Hampshire",
-  Actor: "Colin Firth"
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
 },
 {
   location: [34.0522, -118.2437],
-  name: "Los Angeles",
-  Actor: "Jeff Bridges"
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
 },
 {
   location: [34.0195, -118.2437],
-  name: "Santa Monica",
-  Actor: "Sean Penn"
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
 },
 {
   location: [51.5074, -0.1278],
-  name: "London",
-  Actor: "Daniel Day-Lewis"
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
 },
 {
   location: [32.5007, -94.7405],
-  name: "Longview",
-  Actor: "Forest Whitaker"
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
 },
 {
   location: [43.0987, -77.4419],
-  name: "Fairport",
-  Actor: "Phillip Seymour Hoffman"
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
 },
 {
   location: [32.736, -96.2753],
-  name: "Terrell",
-  Actor: "Jamie Foxx"
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
 },
 {
   location: [34.0195, -118.4912],
-  name: "Santa Monica",
-  Actor: "Sean Penn"
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
 },
 {
   location: [42.139, -83.2418],
-  name: "Woodhaven",
-  Actor: "Adrien Brody"
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
 },
 {
   location: [40.9126, -73.8371],
-  name: "Mount Vernon",
-  Actor: "Denzel Washington"
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
 },
 {
   location: [-41.2865, 174.7762],
-  name: "Wellington",
-  Actor: "Russell Crowe"
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
 },
 {
   location: [40.7489, -74.261],
-  name: "South Orange",
-  Actor: "Kevin Spacey"
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
 },
 {
   location: [43.7711, 11.2486],
-  name: "Tuscany",
-  Actor: "Roberto Benigni"
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
 },
 {
   location: [40.2091, -74.0386],
-  name: "Neptune",
-  Actor: "Jack Nicholson"
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
 },
 {
   location: [-27.5598, 151.9507],
-  name: "TooWoomba",
-  Actor: "Geoffrey Rush"
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
 },
 {
   location: [33.7701, -118.1937],
-  name: "Long Beach",
-  Actor: "Nicolas Cage"
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
 },
 {
   location: [37.978, -122.0311],
-  name: "Concord",
-  Actor: "Tom Hanks"
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
 },
 {
   location: [40.8116, -73.9465],
-  name: "Harlem",
-  Actor: "Al Pacino"
+  location1: [41.5532, -70.6086],
+  location2: [41.5532, -70.6086],
+  name: "Falmouth",
+  name1: "Falmouth",
+  name2: "Falmouth",
+  Actor: "Casey Affleck",
+  Actor1: "Casey Affleck",
+  Actor2: "Casey Affleck"
 }
                   
 ];
@@ -145,12 +286,64 @@ var myIcon = L.icon({
     popupAnchor: [-3, -76],
 });
 
+var actormarker = []
+var actressmarker = []
+var directormarker = []
+
 // Loop through the cities array and create one marker for each city, bind a popup containing its name and Actor add it to the map
 for (var i = 0; i < cities.length; i++) {
   var city = cities[i];
-  L.marker(
+  actormarker.push(L.marker(
       city.location, {
       icon: myIcon})
-    .bindPopup("<h1>" + city.name + "</h1> <hr> <h3>Actor " + city.Actor + "</h3>")
-    .addTo(myMap);
+    .bindPopup("<h1>" + city.name + "</h1> <hr> <h3>Actor " + city.Actor + "</h3>"))
+  actressmarker.push(L.marker(
+      city.location1, {
+      icon: myIcon})
+    .bindPopup("<h1>" + city.name1 + "</h1> <hr> <h3>Actress " + city.Actor1 + "</h3>"))
+  directormarker.push(L.marker(
+      city.location2, {
+      icon: myIcon})
+    .bindPopup("<h1>" + city.name2 + "</h1> <hr> <h3>Director " + city.Actor2 + "</h3>"))
 }
+
+// Add a tile layer
+// Add a tile layer
+var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/dark-v9/tiles/256/{z}/{x}/{y}?" +
+"access_token=pk.eyJ1Ijoic29sb21vbm1pbGxlciIsImEiOiJjamd4MDduZ2QxdjNtMnFtcWticW4yZDdrIn0.UzUscd_xQuIDVRDmh2CZjw"
+);
+
+// Define variables for our base layers
+var streetmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/outdoors-v10/tiles/256/{z}/{x}/{y}?" +
+"access_token=pk.eyJ1Ijoic29sb21vbm1pbGxlciIsImEiOiJjamd4MDduZ2QxdjNtMnFtcWticW4yZDdrIn0.UzUscd_xQuIDVRDmh2CZjw"
+);
+var Actor = L.layerGroup(actormarker);
+var Actress = L.layerGroup(actressmarker);
+var Director = L.layerGroup(directormarker);
+
+
+// Create a baseMaps object
+var baseMaps = {
+  "Street Map": streetmap,
+  "Dark Map": darkmap
+};
+
+// Create an overlay object
+var overlayMaps = {
+  "Actor Home": Actor,
+  "Actress Home": Actress,
+  "Director Home": Director
+};
+
+// Create a map object
+var myMap = L.map("map", {
+  center: [37.983810, 23.727539],
+  zoom: 2,
+  layers: [darkmap, Actor, Actress, Director]
+});
+
+// Pass our map layers into our layer control
+// Add the layer control to the map
+L.control.layers(baseMaps, overlayMaps, {
+  collapsed: false
+}).addTo(myMap);
